a06e9dc5988d012f93b57b1448551dfb9a48f344
Sunil <sunil.nunubilli@gmail.com>
2024-10-30 15:11:15 +0530

    shell parctice

--- k8-WSInstall.sh
+++ k8-WSInstall.sh
@@ -2,4 +2,4 @@
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-Sudo mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
